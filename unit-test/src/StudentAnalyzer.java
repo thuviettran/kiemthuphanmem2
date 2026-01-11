6c7312fca0dd2d7fe034640bcf5f160581d02dc1
Tran Viet Thu <tvthuwork@gmail.com>
2026-01-11 17:50:31 +0700

    feat: implement countExcellentStudents() #1

--- unit-test/src/StudentAnalyzer.java
+++ unit-test/src/StudentAnalyzer.java
@@ -1,10 +1,23 @@
+import java.util.List;
 
 public class StudentAnalyzer {
 
     public int countExcellentStudents(List<Double> scores) {
+        if (scores == null || scores.isEmpty()) {
+            return 0;
+        }
+
+        int count = 0;
+        for (double score : scores) {
+            if (score >= 0 && score <= 10) {     
+                if (score >= 8.0) {
+                    count++;
+                }
+            }
+        }
+        return count;
         
     }
-
     public double calculateValidAverage(List<Double> scores) {
     }
 }
