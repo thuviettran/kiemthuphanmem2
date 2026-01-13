20318f1dd987ef2c748071a2c4681c0d7996bf49
Tran Viet Thu <tvthuwork@gmail.com>
2026-01-13 20:00:22 +0700

    Add Cypress E2E tests for SauceDemo

---
+++ my_cypress/cypress/e2e/checkout_spec.cy.js
@@ -0,0 +1,26 @@
+describe('Checkout Test', () => {
+
+  beforeEach(() => {
+    cy.visit('https://www.saucedemo.com');
+    cy.get('#user-name').type('standard_user');
+    cy.get('#password').type('secret_sauce');
+    cy.get('#login-button').click();
+
+    cy.get('.inventory_item').first()
+      .find('.btn_inventory')
+      .click();
+
+    cy.get('.shopping_cart_link').click();
+    cy.get('[data-test="checkout"]').click();
+  });
+
+  it('Should proceed to checkout step two', () => {
+    cy.get('[data-test="firstName"]').type('John');
+    cy.get('[data-test="lastName"]').type('Doe');
+    cy.get('[data-test="postalCode"]').type('12345');
+    cy.get('[data-test="continue"]').click();
+
+    cy.url().should('include', '/checkout-step-two.html');
+  });
+
+});
